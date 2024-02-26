4d7ef61a94ec014c525539c8ce5ccab1f5621fe7
Rafael Franco <rafaelfariafranco30@gmail.com>
2024-02-26 12:06:27 -0300

    Add files via upload

---
+++ Projeto_helloworld/src/Projeto_helloworld.c
@@ -0,0 +1,27 @@
+/*
+ ============================================================================
+ Name        : Projeto_helloworld.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#define PI 3.14159
+
+int main (void) {
+
+	int idade = 18;
+	const int MAX_NUM = 100;
+
+	printf("variável int comum = %d/n", idade);
+	printf("variável int constante = %d/n", MAX_NUM);
+	printf("variavel constante = %f/n", PI);
+
+	 MAX_NUM = 99;
+
+	return EXIT_SUCCESS;
+}
